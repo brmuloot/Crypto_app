fed5e33e52c10d17eacccd8ac0de44447f4748d2
Brice Mulot <mulotbrice2@gmail.com>
2023-03-14 16:16:01 +0100

    mise a jour du test_pep8

--- test_app/test_pep8.py
+++ test_app/test_pep8.py
@@ -1,7 +1,7 @@
-import flake8.api
+import flake8.api.legacy
 import glob
 
 def test_flake8():
-    style_guide = flake8.api.StyleGuide()
+    style_guide = flake8.api.legacy.StyleGuide()
     report = style_guide.check_files(glob.glob('crypto_app/*.py'))
     assert report.total_errors == 0, 'Code non conforme à PEP8'
--- test_app/verification_pep8.py
+++
@@ -1,7 +0,0 @@
-import flake8.api
-import glob
-
-def test_flake8():
-    style_guide = flake8.api.StyleGuide()
-    report = style_guide.check_files(glob.glob('crypto_app/*.py'))
-    assert report.total_errors == 0, 'Code non conforme à PEP8'
