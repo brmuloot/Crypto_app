ede032f705362cd0fe05ade94d8f93715087c8c6
Brice Mulot <mulotbrice2@gmail.com>
2023-03-14 16:21:14 +0100

    bug fixed

--- test_app/test_pep8.py
+++ test_app/test_pep8.py
@@ -4,4 +4,4 @@ import glob
 def test_flake8():
     style_guide = flake8.api.legacy.StyleGuide()
     report = style_guide.check_files(glob.glob('crypto_app/*.py'))
-    assert report.total_errors == 0, 'Code non conforme à PEP8'
+    assert report.total_errors == 0, '/!\ Code non conforme à PEP8'
