fd4081db05660e414078da5255bd3a02b9442c4b
Brice Mulot <mulotbrice2@gmail.com>
2023-03-14 17:01:31 +0100

    correction du test de blowfish

--- test_app/test_blowfish.py
+++ test_app/test_blowfish.py
@@ -1,13 +1,9 @@
 from crypto_app.blowfish_algo import Blowfish
 
-def test_caesercipher():
-    des = Blowfish()
-    
-    key = "my_secret"
-    message = "This is a secret message"
-    crypt = "4c2b2e4c1fae6deae8c93d5106d13e6b"
-
-    crypted = des.encrypt(message, key)
-    assert crypted == "4c2b2e4c1fae6deae8c93d5106d13e6b"
-    decrypted = des.decrypt(crypt, key)   
-    assert decrypted == "False"
+def test_blowfish():
+    caesercipher = Blowfish()
+    key = "ceciestuneclef"
+    message = "Bonsoir"
+    crypted = caesercipher.encrypt(message, key)
+    decrypted = caesercipher.decrypt(crypted, key)   
+    assert decrypted == "Bonsoir"
